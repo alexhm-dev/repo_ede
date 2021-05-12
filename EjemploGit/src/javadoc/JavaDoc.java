cedf9e2561d97031c994a32bc65e71c7fc2dafd7
Alex <lexurt@gmail.com>
2021-05-12 12:38:50 +0200

    Cambiado el comentario del método nombreMetodo

--- EjemploGit/src/javadoc/JavaDoc.java
+++ EjemploGit/src/javadoc/JavaDoc.java
@@ -6,7 +6,7 @@ public class JavaDoc {
 	 * @version v1.0
 	 * @since 2021/05/07
 	 * @param var1 es un número entero
-	 * @param var2 es un booleano que no se usa
+	 * @param var2 es un booleano que recoge si var1 es o no par
 	 * @return <i>true</i> si es un número par y <i>false</i> si es impar
 	 */
 	public boolean nombreMetodo(int var1, boolean var2) {
